379a43ac1efdf4938fbd29323bcc88bf03f89929
Nini <127434972+ST10396650@users.noreply.github.com>
2025-09-24 15:21:56 +0200

    INITIAL COMMIT

---
+++ BACKEND/server.mjs
@@ -0,0 +1,13 @@
+import https from "https";
+import fs from "fs";
+import app from "./app.mjs";
+const PORT = 3000;
+
+
+const server = https.createServer({
+    key: fs.readFileSync('keys/privatekey.pem'),
+    cert: fs.readFileSync('keys/certificate.pem')
+}, app)
+
+
+app.listen(PORT)
