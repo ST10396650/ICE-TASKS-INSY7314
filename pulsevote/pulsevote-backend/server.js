c8d63d9b4103bf34ea0cc476976fdfd5a6b3e015
Nini <127434972+ST10396650@users.noreply.github.com>
2025-09-25 14:35:07 +0200

    02-Adding SSL

--- pulsevote/pulsevote-backend/server.js
+++ pulsevote/pulsevote-backend/server.js
@@ -1,14 +1,21 @@
 const mongoose = require('mongoose'); // this will be used later
 const app = require('./app');
+const https = require('https');
+const fs = require('fs');
 require('dotenv').config();
 
 const PORT = process.env.PORT || 5000;
+
+const options = {
+  key: fs.readFileSync('ssl/key.pem'),
+  cert: fs.readFileSync('ssl/cert.pem'),
+};
 
 // Test endpoint
 app.get('/test', (req, res) => {
   res.json({ message: "Hello, backend", status: "success" });
 });
 
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+https.createServer(options, app).listen(PORT, () => {
+  console.log('Server running at https://localhost:' + PORT);
 });
